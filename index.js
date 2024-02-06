50d11dd372310c5bbe5a78dd0a0692594e6b3b5c
makeda banza <makedabanza16@gmail.com>
2024-02-06 14:48:02 +0200

    create CRUD

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const express = require('express')
 const app = express()
 const Port=3000;
-app.get('/', function (req, res) {
-  res.send('Hello World')
+let tabs=[
+  {id:1, name:'Makeda', user:'Makban16', password:'Makban@'},
+  {id:2, name:'Eda', user:'Eda16', password:'Eda@'},
+  {id:3, name:'Mak', user:'Mak16', password:'Mak@'},
+]
+app.get('/', (req, res) => {
+  res.send(tabs)
 })
-console.log('first test');
-app.listen(Port, `le server est demmaré au Port ${Port}`)
+app.get('/tweet/:id', (req, res) => {
+  const Id=req.params.id
+  res.send(tabs.filter((e)=>{
+    return e.id==Id
+  }))
+})
+app.delete('/tweet/:id', (req, res) => {
+  const Id=req.params.id
+  res.send(tabs.filter((e)=>{
+    return tabs.splice(Id-1, 1)
+  }))
+})
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  tabs.push(req.body)
+  res.send("Tweet posté avec succès")
+})
+app.put('/tweet/:id', (req, res) => {
+  const Id=req.params.id
+  res.send(tabs.filter((e)=>{
+    return tabs.splice(Id-1, 1,req.body)
+  }))
+})
+app.listen(Port, ()=>{
+  console.log(`le server est demmaré au Port ${Port}`);
+})
